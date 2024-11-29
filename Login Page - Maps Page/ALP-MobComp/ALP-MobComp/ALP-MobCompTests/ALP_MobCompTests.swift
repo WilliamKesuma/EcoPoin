2258d01fc4e752f5a0159e8f84b1a5b202ff329d
Belinda <121992274+belinda2201@users.noreply.github.com>
2024-11-29 11:43:21 +0700

    login, maps, signup

---
+++ Login Page - Maps Page/ALP-MobComp/ALP-MobComp/ALP-MobCompTests/ALP_MobCompTests.swift
@@ -0,0 +1,17 @@
+//
+//  ALP_MobCompTests.swift
+//  ALP-MobCompTests
+//
+//  Created by student on 21/11/24.
+//
+
+import Testing
+@testable import ALP_MobComp
+
+struct ALP_MobCompTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
